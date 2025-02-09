c4bc1d19b098af167cf80cff212509869adab652
charan <sravanamcharan20@gmail.com>
2025-02-09 23:21:59 +0530

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from './routes/user.route.js'
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/user", userRoute);
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
